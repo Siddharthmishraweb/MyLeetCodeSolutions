6d0400a524a23b061ed76ee74e64da481cb3fcd2
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-01 13:11:07 +0530

    Update the-celebrity-problem.cpp

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -4,31 +4,62 @@ class Solution
     //Brute Force
     int celebrity(vector<vector<int> >& M, int n) 
     {
-        vector<int> contender;
-        int zeroCnt = 0;
+        //BRUTE FORCE
+        // vector<int> contender;
+        // int zeroCnt = 0;
+        // for(int i = 0 ; i < n ; i++){
+        //     int cnt = 0;
+        //     for(int j =  0; j < n ; j++){
+        //         if(M[i][j] == 0) zeroCnt++;
+        //         if(i != j){
+        //             if(M[i][j] == 0){
+        //                 cnt++;
+        //             }
+        //             if(cnt == n-1){
+        //                 contender.push_back(i);
+        //             }
+        //         }
+        //     }
+        // }
+        // for(int i = 0 ; i < contender.size() ; i++){
+        //      int cnt = 0;
+        //     for(int row = 0 ; row < n ; row++){
+        //       int col = contender[i];
+        //       if(row != col && M[row][col] == 1) cnt++;
+        //       if(cnt == n - 1) return col;
+        //     }
+        // }
+        // return -1;
+        
+        //OPTIMISED
+        stack<int> st;
+        for(int i = 0 ; i < n ; i++) st.push(i);
+        
+        while(st.size() > 1 ){
+            int a = st.top();
+            st.pop();
+            int b = st.top();
+            st.pop();
+            if(M[a][b] == 1)
+              st.push(b);
+            else
+               st.push(a);
+        }
+        int potentialCandidate = st.top();
+        int cnt = 0;
         for(int i = 0 ; i < n ; i++){
-            int cnt = 0;
-            for(int j =  0; j < n ; j++){
-                if(M[i][j] == 0) zeroCnt++;
-                if(i != j){
-                    if(M[i][j] == 0){
-                        cnt++;
-                    }
-                    if(cnt == n-1){
-                        contender.push_back(i);
-                    }
-                }
+            if(potentialCandidate != i){
+                if(M[i][potentialCandidate] == 1) cnt++;
             }
         }
-        for(int i = 0 ; i < contender.size() ; i++){
-             int cnt = 0;
-            for(int row = 0 ; row < n ; row++){
-              int col = contender[i];
-              if(row != col && M[row][col] == 1) cnt++;
-              if(cnt == n - 1) return col;
+        // check if potential one is really potential if all are one in potenial row excet for potential itself
+        int oneZero = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(potentialCandidate != i){
+                if(M[potentialCandidate][i] == 0) oneZero++;
             }
         }
-        return -1;
+        return oneZero == n - 1 && cnt == n - 1 ? potentialCandidate : -1;
     }
 };
 
