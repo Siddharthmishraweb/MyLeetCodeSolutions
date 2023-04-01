cfac31a70ee762810cca565fff4d403812b97277
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-01 11:41:52 +0530

    Update the-celebrity-problem.cpp

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -1,12 +1,3 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-//User function template for C++
-
 class Solution 
 {
     public:
@@ -29,59 +20,18 @@ class Solution
                 }
             }
         }
-
-        // cout<<"con are : "<<endl;
-        // for(int i = 0 ; i < contender.size() ; i++) cout<<contender[i]<<", ";
-        // cout<<endl<<":ended"<<endl;
-        // all condenders we have
         for(int i = 0 ; i < contender.size() ; i++){
              int cnt = 0;
             for(int row = 0 ; row < n ; row++){
               int col = contender[i];
               if(row != col && M[row][col] == 1) cnt++;
-              //cout<<"cnt: "<<cnt<<endl;
               if(cnt == n - 1) return col;
             }
         }
         return -1;
-        if(zeroCnt == n* n) return -1;
-        return contender.size() > 0 ? contender[0] : -1;;
     }
 };
 
-
-
-
-// 5
-// 4
-// 0 0 0 1
-// 1 0 1 1
-// 1 0 0 1
-// 0 0 0 0
-// 5
-// 0 0 1 1 1
-// 1 0 0 1 1
-// 0 0 0 0 1
-// 0 1 1 0 1
-// 0 0 0 0 0
-// 4
-// 0 1 0 1
-// 0 0 0 1
-// 1 0 0 1
-// 0 0 0 0
-// 4
-// 0 0 0 0
-// 1 0 1 1
-// 1 0 0 0
-// 1 0 1 0
-// 5
-// 0 1 1 1 1
-// 0 0 0 1 1
-// 0 0 0 1 1
-// 0 1 0 0 1
-// 0 0 0 0 0
-
-//{ Driver Code Starts.
 
 int main()
 {
