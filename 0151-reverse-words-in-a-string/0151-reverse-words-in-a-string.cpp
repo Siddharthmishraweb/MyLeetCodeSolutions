7ecc802d1f253035246064aa4edbe7d3f6aa1d47
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-01 20:53:26 +0530

    Time: 9 ms (34.71%), Space: 8.2 MB (41.45%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,58 +1,17 @@
 class Solution {
 public:
-    string removeExtraSpaces(string str){
-        int index1 = 0,index2 = 0;
-        for(int i = 0 ; i < str.size() ; i++){
-            if(str[i] != ' '){ 
-                index1 = i;
-                break;
-            }
-        }
-        for(int i = str.size() - 1; i >= 0 ; i--){
-            if(str[i] != ' '){
-                index2 = i;
-                break;
-            }
-        }
-        if(index2 == str.size() - 1) index2 = index2 + 1;
-        return str.substr(index1,index2);
-    }
     string reverseWords(string s) {
-        int cntSpaces = 0;
-        for(int i = 0 ; i < s.length() ; i++){
-            if(s[i] == ' ') cntSpaces++;
+        stringstream ss(s);
+        string word;
+        vector<string> arr;
+        while(ss >> word){
+            arr.push_back(word);
         }
-        if(cntSpaces == 0) return s;
-        stack<string> st;
-        string temp ="";
-        int index = 0;
-        for(int i = 0 ; i < s.size() ; i++){
-            if(s[i] != ' ')temp += s[i];
-            if(s[i] == ' '){
-                if(temp.size() >= 1)
-                 st.push(temp);
-                temp = "";
-                index = i;
-            }
+        string finalAns = "";
+        for(int i = arr.size() - 1; i >= 0 ; i--){
+            finalAns += arr[i];
+            if(i != 0) finalAns += " ";
         }
-        string norm = "";
-        for(int i = index + 1 ; i < s.size() ; i++){
-            if(s[i] != ' '){
-                norm += s[i];
-            }
-        }
-        if(index != 0){
-            st.push(norm);
-        }
-        int size =st.size();
-        string ansH="";
-        for(int i = 0; i < size ; i++){
-            ansH += st.top();
-            if(i != size -1)
-                ansH += " ";
-            st.pop();
-        }
-        return removeExtraSpaces(ansH);
-        //return ansH;
+        return finalAns;
     }
 };
