ff30390a49370205c1f66d5103946afc1f5e2275
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-03 07:44:17 +0530

    Time: 96 ms (50.99%), Space: 159.5 MB (23.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
+    void check(int& start, int& end, string s,int &maxi, int& index){
+        while((start >= 0 && end < s.length() )&&  s[start] == s[end]){
+            if(end - start + 1 > maxi){
+                maxi = end - start + 1;
+                index = start;
+            }
+            start--;
+            end++;
+        }        
+    }
     string longestPalindrome(string s) {
         if(s.size()==1) return s;
         //for odd length
         int maxi = 1,index = 0;
-        for(int i = 1 ; i < s.length() ; i++){
+        int maxi2 = 1, index2 = 0;
+        for(int i = 0 ; i < s.length() ; i++){
             int start = i - 1;
             int end = i + 1;
             int len = 1;
-            while((start >= 0 && end < s.length() )&&  s[start] == s[end]){
-                if(end - start + 1 > maxi){
-                    maxi = end - start + 1;
-                    index = start;
-                }
-                start--;
-                end++;
-            }
-        }
-        int maxi2 = 1, index2 = 0;
-        //for odd length
-        for(int i = 0 ; i < s.length() ; i++){
-            int start = i;
-            int end = i + 1;
-            while((start >= 0 && end < s.length() )&&  s[start] == s[end]){
-                if(end - start + 1 > maxi2){
-                    maxi2 = end - start + 1;
-                    index2 = start;
-                }
-                start--;
-                end++;
-            }
+            
+            check(start,end,s,maxi,index);
+            start = i;
+            end = i + 1;
+            check(start,end,s,maxi2,index2);
         }
         if(maxi < maxi2){
             index = index2;
