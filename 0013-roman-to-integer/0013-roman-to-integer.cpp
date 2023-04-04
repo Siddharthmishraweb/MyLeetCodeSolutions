75c044f16b5c640e9512730d78ba614d0d61e9fd
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-04 05:41:53 +0530

    Time: 18 ms (45.16%), Space: 10.1 MB (5.00%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -3,53 +3,39 @@ public:
     int romanToInt(string s) {
         unordered_map<string, int> symBol;
         symBol["I"] = 1;
-        symBol["IV"] = 4;
         symBol["V"] = 5;
-        symBol["IX"] = 9;
         symBol["X"] = 10;
-        symBol["XL"] = 40;
         symBol["L"] = 50;
-        symBol["XC"] = 90;
         symBol["C"] = 100;
-        symBol["CD"] = 400;
         symBol["D"] = 500;
-        symBol["CM"] = 900;
         symBol["M"] = 1000;
-        int sumAns = 0;
-        for(int i = 0 ; i < s.length() ; i++){
-            int sum = 0;
-            string ans = "";
-            ans += s[i];
-            if(s[i] == 'I'){
-                if(i+1 < s.length() && s[i+1] == 'V'){
-                    ans += 'IV';
-                    i++;
-                }else if(i+1 < s.length() && s[i+1] == 'X'){
-                    ans += 'IX';
-                    i++;
-                }
-            }
-            if(s[i] == 'X'){
-                if(i+1 < s.length() && s[i+1] == 'L'){
-                    ans += 'XL';
-                    i++;
-                }else if(i+1 < s.length() && s[i+1] == 'C'){
-                    ans += 'XC';
-                    i++;
-                }                
-            }
-            if(s[i] == 'C'){
-                if(i+1 < s.length() && s[i+1] == 'D'){
-                    ans += 'CD';
-                    i++;
-                }else if(i+1 < s.length() && s[i+1] == 'M'){
-                    ans += 'CM';
-                    i++;
-                }                  
-            }
-            sumAns += symBol[ans];
-            cout<<ans<<endl;
+        if(s.size() == 1){
+            string newStr = "";
+            newStr += s[0];
+            return symBol[newStr];
         }
+        int sumAns = 0;
+        int index = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(i + 1 < s.length()){
+                string ith = "",ith1 = "";
+                ith += s[i];
+                ith1 += s[i+1];
+                int a = symBol[ith];
+                int b = symBol[ith1];
+                if(a < b) a =  -1 * a;
+                sumAns += a;
+                index = i;
+            }
+        }
+        if(index < s.length()-1){
+            index++;
+            string newStr = "";
+            newStr += s[index];
+            sumAns += symBol[newStr];
+        }
+            
+        cout<<index;
         return sumAns;
     }
 };
