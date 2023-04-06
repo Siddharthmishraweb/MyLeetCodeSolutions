957128030c688b750950d3ba3647994039702d1e
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-06 06:33:14 +0530

    Time: 4 ms (71.60%), Space: 9 MB (99.93%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,30 @@
+#define ll long long
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        ll len = INT_MAX,index = 0;
+        string str = "";
+        for(int i = 0 ; i < strs.size() ; i++){
+            if(strs[i].size() < len){
+                len = strs[i].size();
+                str = strs[i];
+                index =i;
+            }
+        }
+        int cnt = 0;
+        bool isValid = true;
+        for(int i = 0 ; i < str.size() ; i++){
+            for(int j = 0 ; j < strs.size() ; j++){
+                if(j == index) continue;
+                if(strs[j][i] != str[i]){
+                    isValid = false;
+                }
+            }
+            if(isValid){
+                cnt++;
+            }else break;
+        }
+        return cnt == 0?"": str.substr(0,cnt);
+
+    }
+};
