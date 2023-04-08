7b2c9cd8045d6361b357a9f157faf302ecab8e44
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-08 18:01:36 +0530

    Time: 1070 ms (5.18%), Space: 16.3 MB (85.71%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int lps[s.size()];
+        lps[0] = 0;
+        int len = 0;
+        for(int i = 0 ; i < s.size() ;i++) lps[i]=0;
+        for(int i = 1 ; i < s.size() ; i++){
+            if(s[len] == s[i]){
+                len++;
+                lps[i] = len;
+            }else{
+               if(len == 0) lps[i] = 0;
+               else{
+                   len =lps[len-1];
+                   i--;
+               } 
+                    
+            }
+            
+        }
+        for(int i = 0 ; i < s.size() ; i++){
+            cout<<lps[i]<<" ";
+        }
+        string ans = "";
+        for(int i = 0 ; i < lps[s.size() - 1] ; i++){
+            ans += s[i];
+        }
+        return ans;
+    }
+};
