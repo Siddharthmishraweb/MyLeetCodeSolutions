5f747895c7d2b31397c8f7e46034dd8558b2b3b7
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-08 18:51:46 +0530

    Time: 4 ms (38.77%), Space: 6.3 MB (31.28%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,11 +1,51 @@
 class Solution {
 public:
+    
+    void lpsGenerator(string s, int* lps){
+        // int lps[s.size()];
+        lps[0] = 0;
+        int len = 0;
+        for(int i = 1 ; i < s.size() ; i++){
+            if(s[len] == s[i]){
+                len++;
+                lps[i] = len;
+            }else{
+               if(len == 0) lps[i] = 0;
+               else{
+                   len = lps[len-1];
+                   i--;
+               }
+            }
+        }        
+    }
     int strStr(string haystack, string needle) {
+        int ans =-1;
+        //using KMP Algo
         int size = needle.size();
-        for(int i = 0 ; i < haystack.size() ; i++){
-            if(haystack.substr(i,size) == needle)
-                return i;
+        int n = haystack.size();
+        int lps[size];
+        lpsGenerator(needle,lps);
+        int i = 0 , j = 0;
+        while(i < n){
+            if(haystack[i] == needle[j]){
+                i++;
+                j++;
+                if(j == size){
+                    // j = lps[j-1];
+                    ans =  i - j;
+                    j = lps[j-1];
+                    break;
+                }
+            }else if(i < n ){
+                if(j == 0) i++;
+                else j = lps[j-1];
+            }
         }
-        return -1;
+        
+        // for(int i = 0 ; i < haystack.size() ; i++){
+        //     if(haystack.substr(i,size) == needle)
+        //         return i;
+        // }
+         return ans;
     }
 };
