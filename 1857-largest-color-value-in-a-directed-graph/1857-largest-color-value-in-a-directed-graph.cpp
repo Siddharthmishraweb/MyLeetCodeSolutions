1db7efebebbed47398f7c7764c013b592707fafa
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-09 08:09:53 +0530

    Time: 609 ms (76.07%), Space: 170.1 MB (58.55%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -6,7 +6,7 @@ class Solution {
     int processed = 0;
     vector<vector<int>> graph(n);
     vector<int> inDegree(n);
-    queue<int> q;
+    queue<int> pendingEdges;
     vector<vector<int>> count(n, vector<int>(26));
 
     // Build graph
@@ -20,18 +20,18 @@ class Solution {
     //Simple Code 
     for (int i = 0; i < n; ++i)
       if (inDegree[i] == 0)
-        q.push(i);
+        pendingEdges.push(i);
 
-    while (!q.empty()) {
-      const int out = q.front();
-      q.pop();
+    while (!pendingEdges.empty()) {
+      const int out = pendingEdges.front();
+      pendingEdges.pop();
       ++processed;
       ans = max(ans, ++count[out][colors[out] - 'a']);
       for (const int in : graph[out]) {
         for (int i = 0; i < 26; ++i)
           count[in][i] = max(count[in][i], count[out][i]);
         if (--inDegree[in] == 0)
-          q.push(in);
+         pendingEdges.push(in);
       }
     }
 
