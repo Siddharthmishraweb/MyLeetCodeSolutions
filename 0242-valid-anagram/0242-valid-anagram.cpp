a95435cff519c06e24577f5d38f45438fc2588dd
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-10 08:00:24 +0530

    Time: 15 ms (41.21%), Space: 7.3 MB (84.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> a, b;
-        for(int i = 0 ; i < s.size() ; i++) a.push_back(s[i]);
-        for(int i = 0 ; i < t.size() ; i++) b.push_back(t[i]);
-        if(a.size() != b.size()) return false;
-        sort(a.begin(), a.end());
-        sort(b.begin(), b.end());
-        for(int i = 0 ; i < a.size(); i++){
-            if(a[i] != b[i]) return false;
-        }
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
+        // vector<int> a, b;
+        // for(int i = 0 ; i < s.size() ; i++) a.push_back(s[i]);
+        // for(int i = 0 ; i < t.size() ; i++) b.push_back(t[i]);
+        // if(a.size() != b.size()) return false;
+        // sort(a.begin(), a.end());
+        // sort(b.begin(), b.end());
+        // for(int i = 0 ; i < a.size(); i++){
+        //     if(a[i] != b[i]) return false;
+        // }
+        // return true;
     }
 };
