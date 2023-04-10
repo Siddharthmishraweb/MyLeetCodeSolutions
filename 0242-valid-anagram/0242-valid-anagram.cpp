918b5d863aba0d4eac328b508608093c3f4d2e54
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-10 08:05:44 +0530

    Time: 8 ms (74.89%), Space: 7.5 MB (16.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
+    // bool isAnagram(string s, string t) {
+    //     sort(s.begin(), s.end());
+    //     sort(t.begin(), t.end());
+    //     return s == t;
+    //     // vector<int> a, b;
+    //     // for(int i = 0 ; i < s.size() ; i++) a.push_back(s[i]);
+    //     // for(int i = 0 ; i < t.size() ; i++) b.push_back(t[i]);
+    //     // if(a.size() != b.size()) return false;
+    //     // sort(a.begin(), a.end());
+    //     // sort(b.begin(), b.end());
+    //     // for(int i = 0 ; i < a.size(); i++){
+    //     //     if(a[i] != b[i]) return false;
+    //     // }
+    //     // return true;
+    // }
+    
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
-        // vector<int> a, b;
-        // for(int i = 0 ; i < s.size() ; i++) a.push_back(s[i]);
-        // for(int i = 0 ; i < t.size() ; i++) b.push_back(t[i]);
-        // if(a.size() != b.size()) return false;
-        // sort(a.begin(), a.end());
-        // sort(b.begin(), b.end());
-        // for(int i = 0 ; i < a.size(); i++){
-        //     if(a[i] != b[i]) return false;
-        // }
-        // return true;
-    }
+        if(s.size() != t.size()) return false;
+        unordered_map<char,int> map;
+        for(int i = 0 ; i < s.size() ; i++){
+            map[s[i]]++;
+            map[t[i]]--;
+        }
+        for(auto x: map){
+            if(x.second != 0) return false;
+        }
+        return true;
+    }    
 };
