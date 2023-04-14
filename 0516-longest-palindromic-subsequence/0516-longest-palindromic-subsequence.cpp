31a88c2ad4a420d3ff335293a61bb0739381c1fb
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-14 05:38:17 +0530

    Time: 70 ms (93.97%), Space: 10.8 MB (79.61%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,71 +1,25 @@
 class Solution {
 public:
-    // //recursive solution
-    // int lcs(string s1,string s2, int m, int n){
-    //     // base condition
-    //     if(m == 0 || n == 0) return 0;
-    //     if(s1[m-1] == s2[n-1]) return 1 + lcs(s1,s2,m-1,n-1);
-    //     else{
-    //         return max(lcs(s1,s2,m-1,n), lcs(s1,s2,m,n-1));
-    //     }
-    //     return 0;
-    // }
-    // int longestPalindromeSubseq(string s) {
-    //     string s2 = s;
-    //     reverse(s2.begin(), s2.end());
-    //     return lcs(s,s2 ,s.size(),s.size());
-    // }
-
-    
-    
-    //memoization solution
-//     int dp[1000][1000];
-//     int lcs(string s1,string s2, int m, int n){
-
-//         // base condition
-//         if(m == 0 || n == 0) return 0;
-        
-//         if(dp[m][n] != -1) return dp[m][n];
-        
-//         if(s1[m-1] == s2[n-1]){
-//             dp[m][n] = 1 + lcs(s1,s2,m-1,n-1);
-//         }
-//         else{
-//             dp[m][n] =  max(lcs(s1,s2,m-1,n), lcs(s1,s2,m,n-1));
-//         }
-
-//         // return dp[m][n] = s1[m-1] == s2[n-1] ? 1 + lcs(s1,s2,m-1,n-1) : max(lcs(s1,s2,m-1,n), lcs(s1,s2,m,n-1));
-        
-//     }
-
-
-    //Top down approch DP solution
-    int lcs(string s1,string s2, int m, int n){
+    int lcs(string a, string b, int m, int n){
         int dp[m+1][n+1];
-        for(int i = 0 ; i < m+1 ; i++){
+        for(int i = 0 ; i < m ; i++){
             dp[i][0] = 0;
             dp[0][i] = 0;
         }
-        for(int i = 1 ; i < m+ 1; i++){
-            for(int j = 1 ;j < n + 1 ; j++){
-                if(s1[i-1] == s2[j-1]){
-                    dp[i][j] = 1+ dp[i-1][j-1];
+        for(int i = 1; i < m + 1; i++){
+            for(int j = 1; j < n + 1; j++){
+                if(a[i-1] == b[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 }else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
         return dp[m][n];
     }
-
     int longestPalindromeSubseq(string s) {
         string s2 = s;
-        // int dp[s.size()+1][s.size()+1];
-        //memset(dp, -1,sizeof(dp));
-        reverse(s2.begin(), s2.end());
-        return lcs(s,s2 ,s.size(),s.size());
-    }    
-    
-    
-    
+        reverse(s.begin(), s.end());
+        return lcs(s2,s,s.size(), s.size());
+    }
 };
