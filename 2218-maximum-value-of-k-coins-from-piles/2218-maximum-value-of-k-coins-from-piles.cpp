a67798074657f660ebfdf7547ea0848723f4c3c7
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-16 08:55:12 +0530

    Time: 354 ms (35.77%), Space: 18.2 MB (50.91%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
-    int n;
-    int dp[1001][2001];
-    int solve(vector<vector<int>>& piles, int i, int k){
-        if(i >= n) return 0;
+    // i => ith piles
+    int n = 0;
+    //int dp[1001][1001];
+    int solve(vector<vector<int>>& piles, int i, int k, vector<vector<int>>& dp){
         if(dp[i][k] != -1) return dp[i][k];
-        int not_taken = solve(piles, i+1, k );
-        int sum = 0, taken = 0;
-        for(int j = 0 ; j < min((int)piles[i].size(), k); j++){
+        //base case 
+        if(i >= piles.size()) return 0;
+        if(k <= 0) return 0;
+        // not taken
+        int not_taken = solve(piles, i+1, k , dp);
+        
+        // taken
+        int sum = 0, maxR = 0;
+        //cout<<piles[i].size();
+        for(int j = 0 ; j < min(k,  (int)piles[i].size() ); j++){
+            // take from ith piles only
             sum += piles[i][j];
-            taken = max(taken, sum + solve(piles, i+1, k - (j + 1)));
+            // take from other piles as well
+            int money = 0;
+            if(k - (j + 1) >= 0){
+                money = sum + solve(piles, i+1, k - (j + 1), dp);
+            }
+                
+            maxR = max(maxR, money);
         }
-        return dp[i][k] = max(taken, not_taken);
+       // return max(maxR, not_taken);
+        
+        return dp[i][k] = max(maxR, not_taken);
     }
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
-        if(k == 0) return 0;
+        //memset(dp, -1, sizeof(dp));
         n = piles.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(piles, 0, k);
+       vector<vector<int>> dp(n+1,vector<int>(k+1,-1));
+        n = piles.size();
+        return solve(piles, 0, k, dp);
     }
 };
