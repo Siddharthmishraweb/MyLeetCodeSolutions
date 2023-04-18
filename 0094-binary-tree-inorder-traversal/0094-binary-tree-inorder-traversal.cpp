f220db6e389f735fee56c57c8877aa44eb478424
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-04-18 06:10:58 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (45.15%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,21 +12,31 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> inOrder;
-        TreeNode* node = root;
-        stack<TreeNode*> stack;
-        while(true){
-            if(node != NULL){
-                stack.push(node);
-                node = node -> left;
+        //Morris Inorder Traversal => using threaded binary tree
+        vector<int> inorder;
+        TreeNode* curr = root;
+        while(curr != NULL){
+            if(curr -> left == NULL){
+                inorder.push_back(curr -> val);
+                curr = curr -> right;
             }else{
-                if(stack.empty()) break;
-                node = stack.top();
-                inOrder.push_back(node ->val);
-                stack.pop();
-                node = node  -> right;
+                TreeNode* prev = curr -> left;
+                // find the right most guy
+                while(prev -> right != NULL && prev -> right != curr){
+                    prev = prev -> right;
+                }
+                // if threaded connection doesnt already exist
+                if(prev -> right == NULL){
+                    prev -> right = curr;
+                    curr = curr -> left;
+                }//if threded connection already exist means we travessed the left part then remove connection
+                else{
+                    inorder.push_back(curr -> val);
+                    prev -> right = NULL;
+                    curr = curr -> right;
+                }
             }
         }
-        return inOrder;
+        return inorder;
     }
 };
