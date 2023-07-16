fc81b314bf3d6acc042b7a96bf1c6559c02cef6d
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2023-07-16 20:01:16 +0530

    Added solution - LeetHub

--- Minimum swaps and K together - GFG/minimum-swaps-and-k-together.cpp
+++ Minimum swaps and K together - GFG/minimum-swaps-and-k-together.cpp
@@ -9,67 +9,66 @@ using namespace std;
 
 // } Driver Code Ends
 
-// class Solution
-// {
-// public:
-//     int minSwap(int arr[], int n, int k) {
-//         int numLessThank = 0, ans = INT_MAX;
-//         for(int i = 0 ; i < n ; i++)
-//             if(arr[i] <= k) numLessThank++;
-//         int bad = 0;
-//         int i = 0, j = 0;
-//         while(i < n && j < n){
-//             int windowSize = j - i + 1;
-//             if(windowSize < numLessThank){
-//                 if(arr[j] > k) bad++; 
-//                 j++;
-//             }
-//             if(windowSize == numLessThank){
-//                 ans = min(ans, bad);
-//                 j++;
-//                 if(arr[j] > k) bad++;
-//                 if(arr[i] > k) bad--;
-//                 i++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-class Solution {
+class Solution
+{
 public:
     int minSwap(int arr[], int n, int k) {
-        int numLessThanK = 0, ans = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] <= k) numLessThanK++;
-        }
-        
+        int numLessThank = 0, ans = INT_MAX;
+        for(int i = 0 ; i < n ; i++)
+            if(arr[i] <= k) numLessThank++;
         int bad = 0;
-        int i = 0, j = numLessThanK - 1;
-        
-        // Count the initial number of "bad" elements in the first window
-        for (int x = i; x <= j; x++) {
-            if (arr[x] > k) bad++;
+        for(int i = 0 ; i < numLessThank ; i++){
+            if(arr[i] > k) bad++;
         }
         
-        ans = min(ans, bad);
-        
-        while (j < n - 1) {
-            // Move the window
-            i++;
-            j++;
-            
-            // Update the "bad" count based on the new elements in the window
-            if (arr[j] > k) bad++;
-            if (arr[i - 1] > k) bad--;
-            
-            // Update the answer
-            ans = min(ans, bad);
+        int i = 0, j = numLessThank - 1;;
+        while(i < n && j < n){
+           
+                ans = min(ans, bad);
+                j++;
+                if(arr[j] > k) bad++;
+                if(arr[i] > k) bad--;
+                i++;
+
         }
-        
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int minSwap(int arr[], int n, int k) {
+//         int numLessThanK = 0, ans = INT_MAX;
+//         for (int i = 0; i < n; i++) {
+//             if (arr[i] <= k) numLessThanK++;
+//         }
+        
+//         int bad = 0;
+//         int i = 0, j = numLessThanK - 1;
+        
+//         // Count the initial number of "bad" elements in the first window
+//         for (int x = i; x <= j; x++) {
+//             if (arr[x] > k) bad++;
+//         }
+        
+//         ans = min(ans, bad);
+        
+//         while (j < n - 1) {
+//             // Move the window
+//             i++;
+//             j++;
+            
+//             // Update the "bad" count based on the new elements in the window
+//             if (arr[j] > k) bad++;
+//             if (arr[i - 1] > k) bad--;
+            
+//             // Update the answer
+//             ans = min(ans, bad);
+//         }
+        
+//         return ans;
+//     }
+// };
 
 
 
