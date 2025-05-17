0eebbe3998bd2d4e81e1d71825b3b7bfbf3b4e0a
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2025-05-17 17:01:55 +0530

    Time: 7 ms (5.70%) | Memory: 15.4 MB (91.08%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,31 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-  public:
-   vector<pair<int,pair<int,int >>> verticalMap;
-
-  vector<pair<int,pair<int,int>>> horizontalDepth(TreeNode * root, int hD, int depth) {
-    if (root == NULL) return verticalMap;
-    if (root -> left) horizontalDepth(root -> left, hD - 1, depth + 1);
-    verticalMap.push_back({ hD, { depth, root -> val }});
-    if (root -> right)  horizontalDepth(root -> right, hD + 1, depth + 1);
-    return verticalMap;
-  }
-
-  vector<vector<int>>verticalTraversal(TreeNode * root) {
-    vector < pair < int, pair < int, int >>> hdAns = horizontalDepth(root, 0, 0);
-    sort(hdAns.begin(), hdAns.end());
-    map < int, vector < int >> mapi;
-    for (auto & p: hdAns) {
-      int hD = p.first;
-      int val = p.second.second;
-      mapi[hD].push_back(val);
+public:
+    vector<pair<int, pair<int, int>>> pairVector;
+    void horizontalDepth(TreeNode* root, int depth, int hd){
+        if(!root) return ;
+        pairVector.push_back({ hd , { depth, root -> val }});
+        if(root -> left) horizontalDepth(root -> left, depth + 1, hd - 1);
+        if(root -> right) horizontalDepth(root -> right, depth + 1, hd + 1);
     }
-    vector < vector < int >> finalWalaAns;
-    for (auto a: mapi) {
-      vector < int > innerAns;
-      vector < int > asdf = a.second;
-      for (auto as: asdf) innerAns.push_back(as);
-      finalWalaAns.push_back(innerAns);
+
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> finalAns;
+        horizontalDepth(root, 0, 0);
+        map<int, vector<int>> mapi;
+        sort(pairVector.begin(), pairVector.end());
+        for(auto p: pairVector){
+            cout << p.first <<" , "<<p.second.first <<" , "<< p.second.second<<endl;
+            mapi[p.first].push_back(p.second.second);
+        }
+        for(auto p: mapi){
+            vector<int> ans;
+            finalAns.push_back(p.second);
+        }
+        return finalAns;
     }
-    return finalWalaAns;
-  }
 };
