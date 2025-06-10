f30014e67cba3ec7468dca27c16599931db7c503
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2025-06-10 07:59:43 +0530

    Time: 10 ms (0.93%) | Memory: 18.5 MB (29.80%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool helper(TreeNode* root1, TreeNode* root2){
+        if((root1 == NULL && root2 != NULL) || (root1 != NULL && root2 == NULL)) return false;
+        if(root1 == NULL && root2 == NULL) return true;
+        cout<<"root1 -> val:: "<<root1 -> val <<" , "<<root2->val<<endl;
+        if(root1 -> val != root2 -> val) return false;
+
+         bool isFirst = helper(root1 -> left, root2 -> right);
+         bool isSecond = helper(root1 -> right, root2 -> left);
+         return isFirst && isSecond;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        if(root -> left && root -> right && root -> left -> val != root -> right->val) return false;
+        if(root -> left == NULL && root -> right != NULL || (root -> left != NULL && root -> right == NULL)) return false;
+        
+        return helper(root -> left, root -> right);
+    }
+};
