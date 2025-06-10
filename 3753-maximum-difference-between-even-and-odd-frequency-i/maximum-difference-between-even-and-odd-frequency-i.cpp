ec28b31110bc72464271e032412a3baf05e6a51f
Siddharthmishraweb <72328255+Siddharthmishraweb@users.noreply.github.com>
2025-06-10 08:23:03 +0530

    Time: 7 ms (5.86%) | Memory: 10 MB (27.18%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -5,20 +5,11 @@ public:
         for(auto a: s){
             freq[a]++;
         }
-        int maxOdd = INT_MIN, minOdd=INT_MAX, minEven=INT_MAX, maxEven=INT_MIN;
+        int maxOdd = INT_MIN, minEven=INT_MAX;
         for(auto a: freq){
-            cout<<"a.second:: "<<a.second<<endl;
-            if(a.second%2==0){
-                maxEven = max(maxEven, a.second);
-                minEven = min(minEven, a.second);
-            }else{
-                maxOdd = max(maxOdd, a.second);
-                minOdd = min(minOdd, a.second);
-            }
+            if(a.second%2==0) minEven = min(minEven, a.second);
+            else maxOdd = max(maxOdd, a.second);
         }
-
- 
-
         return maxOdd - minEven;
     }
 };
